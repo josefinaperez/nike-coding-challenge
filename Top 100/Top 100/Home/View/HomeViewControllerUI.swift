38517a6e1c70b45e9e730328adf1946c78a97733
josefinaperez <josefinap92@gmail.com>
2021-01-15 16:43:16 -0300

    Showed home screen at launch

---
+++ Top 100/Top 100/Home/View/HomeViewControllerUI.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewControllerUI.swift
+//  Top 100
+//
+//  Created by Josefina Perez on 15/01/2021.
+//  Copyright © 2021 Josefina perez. All rights reserved.
+//
+
+import Foundation
+
+extension HomeViewController {
+    
+    private func setupUI() {
+        
+    }
+}
